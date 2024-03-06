b5c9c1a6de05d2df1374af9fa78ba7978dcb4a1f
lakshayjuneja003 <lakshayjuneja003@gmail.com>
2024-03-06 07:43:36 -0800

    module 1 added

---
+++ module1/Day_1/datatypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // for integer var num1
+    int num1;
+    num1 = 23;
+    // for float var. num2
+    float num2;
+    num2 = 223.9;
+    // for boolean type
+    bool pass;
+    pass = false;
+    // for char
+    char c;
+    c = 'l';
+
+    int number;
+    // for caching the output - cin
+    cout << "enter a number"<< endl;
+    cin >> number;
+    return 0;
+}
---
+++ module1/Day_1/var.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // var decleration
+    int num1;
+
+    // var initalizing
+    num1 = 23;
+    return 0;
+}
