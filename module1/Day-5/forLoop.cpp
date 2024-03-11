c904b8b34f4605ffd73b0c2a8a5e65f3c357e926
lakshayjuneja003 <lakshayjuneja003@gmail.com>
2024-03-11 18:56:34 +0530

    for loops

---
+++ module1/Day-5/forLoop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout  << "Hello World" << endl;
+    // }
+    // sum from 1 to n
+
+    int sum =  0;
+    for (int i = 1; i <=n; i++)
+    {
+        sum += i;
+    }
+    cout << sum << endl;
+    
+
+}
